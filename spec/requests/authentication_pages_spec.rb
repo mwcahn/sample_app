bb6f26585d441439ba384f9a5bb72c8d796d81c7
Michael Cahn <michael@chewthat.com>
2012-07-20 21:29:36 -0700

    mwcahn Finish edit, update, index, destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
-describe "Authentication" do
-  
-  
-  
-  describe "signin" do
-    
-    
-    
-    describe "with valid information" do
-      
-    
-      
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
       end
     end
   end
-end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,18 +1,31 @@
 require 'spec_helper'
 
 describe "User pages" do
+  
+  
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
     
     
     
     describe "with valid information" do
-      
-      
-      
-      describe "after saving the user" do
-        
-        
-        
-        it { should have_link('Sign out') }
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
       end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
+  end
 end
