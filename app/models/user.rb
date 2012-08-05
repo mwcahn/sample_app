b2c13d581599750818dc7cf8f4030d1b4a5b2ad1
Michael Cahn <michael@chewthat.com>
2012-08-05 00:45:14 -0700

    Trying to add password confirm to user edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :current_password, :password, :password_confirmation, 
   has_secure_password
   has_many :microposts, dependent: :destroy
 
